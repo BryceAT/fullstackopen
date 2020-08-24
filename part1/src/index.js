46e1db200dd2edc009f1265f417873d318122f64
BryceTerwilliger <bryce.terwilliger@gmail.com>
2020-08-23 22:02:52 -0400

    Initial commit

---
+++ part1/src/index.js
@@ -0,0 +1,42 @@
+import React from 'react'
+import ReactDOM from 'react-dom'
+
+const Header = (props) => {
+  return (
+    <h1>{props.course}</h1>
+  )
+}
+const Content = (props) => {
+  return (
+    <p>
+      {props.part} {props.excercise}
+    </p>
+  )
+}
+const Total = (props) => {
+  return (
+    <p>Number of exercises {props.ex1 + props.ex2 + props.ex3}</p>
+  )
+}
+
+const App = () => {
+  const course = 'Half Stack application development'
+  const part1 = 'Fundamentals of React'
+  const exercises1 = 10
+  const part2 = 'Using props to pass data'
+  const exercises2 = 7
+  const part3 = 'State of a component'
+  const exercises3 = 14
+
+  return (
+    <div>
+      <Header course={course} />
+      <Content part={part1} excercise={exercises1} />
+      <Content part={part2} excercise={exercises2} />
+      <Content part={part3} excercise={exercises3} />
+      <Total ex1={exercises1} ex2={exercises2} ex3={exercises3} />
+    </div>
+  )
+}
+
+ReactDOM.render(<App />, document.getElementById('root'))
